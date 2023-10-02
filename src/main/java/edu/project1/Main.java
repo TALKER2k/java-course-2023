8b2a8004e47ae8c7b1d2d3a6c510ed85c336f21f
user <nunikasnikun26504228@yandex.ru>
2023-10-02 21:04:46 +0300

    first test

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -14,7 +14,7 @@ public final class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
+        LOGGER.info("Hello and welcome! New branch hw1!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 0; i <= 2; i++) {

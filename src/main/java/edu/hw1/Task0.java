482635b0cfef8565c901cd933c09b1d80bf42ed7
user <nunikasnikun26504228@yandex.ru>
2023-10-05 00:36:59 +0300

    task 3 4 5 done

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -3,8 +3,6 @@ package edu.hw1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// Press Alt+Enter with your caret at the highlighted text to see how
-// IntelliJ IDEA suggests fixing it.
 public final class Task0 {
     private Task0() {
     }
@@ -17,8 +15,6 @@ public final class Task0 {
      * @param args Аргументы командной строки.
      */
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         LOGGER.info("Привет, мир!");
     }
 }

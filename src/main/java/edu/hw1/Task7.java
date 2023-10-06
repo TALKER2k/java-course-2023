06668d6713e0ee8005fc443f01909c14988c577e
user <nunikasnikun26504228@yandex.ru>
2023-10-07 00:04:42 +0300

    changed task7

--- src/main/java/edu/hw1/Task7.java
+++ src/main/java/edu/hw1/Task7.java
@@ -4,13 +4,20 @@ public final class Task7 {
     private Task7() {}
 
     public static int rotateRight(int n, int shift) {
-        int bits = Integer.SIZE;
-        return (n >>> shift) | (n << (bits - shift));
+        String numberString = Integer.toBinaryString(n);
+        for (int i = 0; i < shift; i++) {
+            numberString = numberString.substring(numberString.length() - 1)
+                    + numberString.substring(0, numberString.length() - 1);
+        }
+        return Integer.parseInt(numberString, 2);
     }
 
     public static int rotateLeft(int n, int shift) {
-        final int bits = 5;
-        int mask = (1 << bits) - 1;
-        return ((n << shift) | (n >>> (bits - shift))) & mask;
+        String numberString = Integer.toBinaryString(n);
+        for (int i = 0; i < shift; i++) {
+            numberString = numberString.substring(1)
+                    + numberString.charAt(0);
+        }
+        return Integer.parseInt(numberString, 2);
     }
 }
--- src/test/java/edu/hw1/Task7Test.java
+++ src/test/java/edu/hw1/Task7Test.java
@@ -9,14 +9,18 @@ public class Task7Test {
     void rotateLeftTest() {
         int number1 = 16;
         int number2 = 17;
+        int number3 = 32432;
         int shift1 = 1;
         int shift2 = 2;
+        int shift3 = 2;
 
         int result1 = Task7.rotateLeft(number1, shift1);
         int result2 = Task7.rotateLeft(number2, shift2);
+        int result3 = Task7.rotateLeft(number3, shift3);
 
         assertThat(result1).isEqualTo(1);
         assertThat(result2).isEqualTo(6);
+        assertThat(result3).isEqualTo(31427);
     }
 
     @SuppressWarnings("checkstyle:MagicNumber")
@@ -24,10 +28,10 @@ public class Task7Test {
     void rotateRightTest() {
         int number1 = 8;
         int number2 = 0;
-        int number3 = 16;
+        int number3 = 3242;
         int shift1 = 1;
         int shift2 = 1;
-        int shift3 = 0;
+        int shift3 = 2;
 
         int result1 = Task7.rotateRight(number1, shift1);
         int result2 = Task7.rotateRight(number2, shift2);
@@ -35,6 +39,6 @@ public class Task7Test {
 
         assertThat(result1).isEqualTo(4);
         assertThat(result2).isEqualTo(0);
-        assertThat(result3).isEqualTo(16);
+        assertThat(result3).isEqualTo(2858);
     }
 }
